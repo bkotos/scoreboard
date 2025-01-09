bea4db9b1154a26ced4cbb672efe63f0e992ebf3
Brian Kotos <brian.kotos@gmail.com>
2025-01-09 09:48:49 -0500

    [red] It should display team 2 with a starting score of 0

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -8,5 +8,14 @@ describe('Scoreboard app', () => {
       cy.contains('Team 1').should('have.id', 'team1')
       cy.contains('0').should('have.attr', 'aria-labelledby', 'team1')
     })
+
+    it('should display team 2 with a starting score of 0', () => {
+      // act
+      cy.visit('http://127.0.0.1:8080/')
+
+      // assert
+      cy.contains('Team 2').should('have.id', 'team2')
+      cy.contains('0').should('have.attr', 'aria-labelledby', 'team2')
+    })
   })
 })
