9a7d76884d61a7a4ae8e5a579e05fb1f1aa19723
Brian Kotos <brian.kotos@gmail.com>
2025-01-10 10:34:08 -0500

    [green] It should display a score of one for team 1 when I click the add button twice and click the subtract button once

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ let score = 0
 document.getElementById('btn-add-team1').onclick = () => {
     document.getElementById('score-team1').innerText = `${++score}`
 }
+document.getElementById('btn-subtract-team1').onclick = () => {
+    document.getElementById('score-team1').innerText = `${--score}`
+}
