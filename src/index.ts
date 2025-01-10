1375fb1d8e2283589fd867302676a50a73b154fd
Brian Kotos <brian.kotos@gmail.com>
2025-01-10 10:28:44 -0500

    [green] It should increase the score by 2 for team 1 when click the add button twice

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+let score = 0
 document.getElementById('btn-add-team1').onclick = () => {
-    document.getElementById('score-team1').innerText = '1'
+    document.getElementById('score-team1').innerText = `${++score}`
 }
