19a2c7bb399ac6f855ff989e9cbf95c6776f7fdd
Brian Kotos <brian.kotos@gmail.com>
2025-01-10 18:33:03 -0500

    Update card styling.

--- src/view.ts
+++ src/view.ts
@@ -3,13 +3,13 @@ import { Team } from "./model"
 export const renderTeamCard = (team: Team) => {
     const html = `
         <div class="card">
-            <div class="card-content">
-                <p class="title" id="title-${team.id}">
+            <div class="card-content has-text-centered">
+                <p class="subtitle mb-5" id="title-${team.id}">
                     <span id="teamName-${team.id}">${team.name}</span>
                     <button id="btn-edit-teamName-${team.id}" class="button is-small" aria-label="Change name of ${team.name}">Edit</button>
                 </p>
-                <input id="edit-teamName-${team.id}" type="text" class="title is-hidden" value="${team.name}" aria-label="Change team name" />
-                <p class="subtitle" aria-labelledby="teamName-${team.id}" id="score-${team.id}">0</p>
+                <input id="edit-teamName-${team.id}" type="text" class="subtitle mb-5 p-0 is-hidden" value="${team.name}" aria-label="Change team name" />
+                <p class="title" aria-labelledby="teamName-${team.id}" id="score-${team.id}">0</p>
             </div>
             <footer class="card-footer">
             <button class="card-footer-item" aria-label="Subtract one point for ${team.name}" id="btn-subtract-${team.id}">
