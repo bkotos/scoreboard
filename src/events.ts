c442f67a4bd9f83d0f2e483878a34540c6e33821
Brian Kotos <brian.kotos@gmail.com>
2025-01-10 18:05:07 -0500

    Move event handlers into named functions.

--- src/events.ts
+++ src/events.ts
@@ -15,27 +15,32 @@ const safelyRenderTeamName = (team: Team) => {
     document.getElementById(`teamName-${team.id}`).innerText = team.name
 }
 
-export const listenToEventsForTeam = (team: Team) => {
-    document.getElementById(`btn-add-${team.id}`).onclick = () => {
-        document.getElementById(`score-${team.id}`).innerText = `${++team.score}`
-    }
-    document.getElementById(`btn-subtract-${team.id}`).onclick = () => {
-        if (team.score > 0) document.getElementById(`score-${team.id}`).innerText = `${--team.score}`
-    }
-    document.getElementById(`btn-edit-teamName-${team.id}`).onclick = () => {
-        enableTeamNameEditing(team.id)
-        focusOnTeamNameInput(team.id)
-    }
-    document.getElementById(`edit-teamName-${team.id}`).onkeydown = (e) => {
-        if (e.code === 'Enter') {
-            disableTeamNameEditing(team.id)
-        }
-    }
-
-    document.getElementById(`edit-teamName-${team.id}`).onblur = () => {
+const onScoreAddClick = (team: Team) => {
+    document.getElementById(`score-${team.id}`).innerText = `${++team.score}`
+}
+const onScoreSubtractClick = (team: Team) => {
+    if (team.score > 0) document.getElementById(`score-${team.id}`).innerText = `${--team.score}`
+}
+const onEditTeamNameClick = (team: Team) => {
+    enableTeamNameEditing(team.id)
+    focusOnTeamNameInput(team.id)
+}
+const onTeamNameKeyDown = (team: Team, e: KeyboardEvent) => {
+    if (e.code === 'Enter') {
         disableTeamNameEditing(team.id)
-        const value = (document.getElementById(`edit-teamName-${team.id}`) as HTMLInputElement).value
-        team.name = value
-        safelyRenderTeamName(team)
     }
+}
+const onTeamNameBlur = (team: Team) => {
+    disableTeamNameEditing(team.id)
+    const value = (document.getElementById(`edit-teamName-${team.id}`) as HTMLInputElement).value
+    team.name = value
+    safelyRenderTeamName(team)
+}
+
+export const listenToEventsForTeam = (team: Team) => {
+    document.getElementById(`btn-add-${team.id}`).onclick = onScoreAddClick.bind(null, team)
+    document.getElementById(`btn-subtract-${team.id}`).onclick = onScoreSubtractClick.bind(null, team)
+    document.getElementById(`btn-edit-teamName-${team.id}`).onclick = onEditTeamNameClick.bind(null, team)
+    document.getElementById(`edit-teamName-${team.id}`).onkeydown = onTeamNameKeyDown.bind(null, team)
+    document.getElementById(`edit-teamName-${team.id}`).onblur = onTeamNameBlur.bind(null, team)
 }
