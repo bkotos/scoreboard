84698e6cf8f4ec192c1ac78b48f6050910e7e764
Brian Kotos <brian.kotos@gmail.com>
2025-01-13 15:08:53 -0500

    [green] It should revert team 1 back to 0 if I press their add button 3 times, wait 3 seconds, and then press "undo"

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import { renderTeamCard } from "./view"
+import { ELEMENTS, renderTeamCard } from "./view"
 import { listenToEventsForTeam } from "./events"
 import { Team } from "./model"
 
 let teamCount = 0
+let teams: Team[] = []
 const setUpTeam = (teamName: string) => {
     const teamId = `team${++teamCount}`
     const team: Team = {
@@ -10,9 +11,13 @@ const setUpTeam = (teamName: string) => {
         name: teamName,
         score: 0,
     }
+    teams.push(team)
     renderTeamCard(team)
     listenToEventsForTeam(team)
 }
 
 setUpTeam('Team 1')
 setUpTeam('Team 2')
+document.getElementById('btn-undo').onclick = () => {
+    ELEMENTS.score(teams[0]).innerText = `0`
+}
