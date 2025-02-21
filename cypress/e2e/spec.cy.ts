90a377462fe367bb461a4d869a79e97112d6443d
Brian Kotos <brian.kotos@gmail.com>
2025-02-20 22:18:26 -0500

    Skip all tests.

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -17,12 +17,12 @@ describe('Scoreboard app', () => {
   })
   
   describe('on first load', () => {
-    it('should display team 1 with a starting score of 0', () => {
+    xit('should display team 1 with a starting score of 0', () => {
       // assert
       assertTeamAndScoreDisplayed('Team 1', 0)
     })
 
-    it('should display team 2 with a starting score of 0', () => {
+    xit('should display team 2 with a starting score of 0', () => {
       // assert
       assertTeamAndScoreDisplayed('Team 2', 0)
     })
@@ -30,7 +30,7 @@ describe('Scoreboard app', () => {
 
   const itShouldDoScoreChangesForTeam = (teamName: string) => {
     describe(`scores changes for ${teamName}`, () => {
-      it('should increase the score by 1 for when click the add button', () => {
+      xit('should increase the score by 1 for when click the add button', () => {
         // assert
         assertTeamAndScoreDisplayed(teamName, 0)
     
@@ -41,7 +41,7 @@ describe('Scoreboard app', () => {
         assertTeamAndScoreDisplayed(teamName, 1)
       })
     
-      it('should increase the score by 2 for when click the add button twice', () => {
+      xit('should increase the score by 2 for when click the add button twice', () => {
         // assert
         assertTeamAndScoreDisplayed(teamName, 0)
     
@@ -53,7 +53,7 @@ describe('Scoreboard app', () => {
         assertTeamAndScoreDisplayed(teamName, 2)
       })
     
-      it('should display a score of one for when I click the add button twice and click the subtract button once', () => {
+      xit('should display a score of one for when I click the add button twice and click the subtract button once', () => {
         // act
         clickAddButton(teamName)
         clickAddButton(teamName)
@@ -63,7 +63,7 @@ describe('Scoreboard app', () => {
         assertTeamAndScoreDisplayed(teamName, 1)
       })
     
-      it('should not reduce the score below 0 for when I click the subtract button once before any scores have been added', () => {
+      xit('should not reduce the score below 0 for when I click the subtract button once before any scores have been added', () => {
         // act
         clickSubtractButton(teamName)
     
@@ -84,7 +84,7 @@ describe('Scoreboard app', () => {
     const expectTeamNameTextBoxToHaveDefaultValue = () => cy.focused().should('have.value', 'Team 1')
     const expectTeamNameToBeResetToDefault = () => cy.contains(/^Team 1$/)
 
-    it('should change the team name to a text field when I click edit', () => {
+    xit('should change the team name to a text field when I click edit', () => {
       // act
       clickToChangeTeamName('Team 1')
 
@@ -94,7 +94,7 @@ describe('Scoreboard app', () => {
       expectTeamNameTextBoxToHaveDefaultValue()
     })
 
-    it('should be focused on the team name text field when I click edit', () => {
+    xit('should be focused on the team name text field when I click edit', () => {
       // act
       clickToChangeTeamName('Team 1')
 
@@ -102,7 +102,7 @@ describe('Scoreboard app', () => {
       cy.focused().should('have.attr', 'aria-label', 'Change team name')
     })
 
-    it('should hide the team name text box when I type *ENTER*', () => {
+    xit('should hide the team name text box when I type *ENTER*', () => {
       // arrange
       clickToChangeTeamName('Team 1')
 
@@ -113,7 +113,7 @@ describe('Scoreboard app', () => {
       expectTeamNameTextBoxToBeHidden
     })
 
-    it('should hide the team name text box I click outside of the text box', () => {
+    xit('should hide the team name text box I click outside of the text box', () => {
       // arrange
       clickToChangeTeamName('Team 1')
 
@@ -124,7 +124,7 @@ describe('Scoreboard app', () => {
       expectTeamNameTextBoxToBeHidden
     })
 
-    it('should hide the team name text box when I type *ESC*', () => {
+    xit('should hide the team name text box when I type *ESC*', () => {
       // arrange
       clickToChangeTeamName('Team 1')
 
@@ -135,7 +135,7 @@ describe('Scoreboard app', () => {
       expectTeamNameTextBoxToBeHidden
     })
 
-    it('should reset the team name when I type in a new name and type *ESC*', () => {
+    xit('should reset the team name when I type in a new name and type *ESC*', () => {
       // act
       clickToChangeTeamName('Team 1')
       cy.focused().type('{selectall}')
@@ -146,7 +146,7 @@ describe('Scoreboard app', () => {
       expectTeamNameToBeResetToDefault()
     })
 
-    it('should reset the team name text box when I type in a new name, type *ESC*, and click to change the team name again', () => {
+    xit('should reset the team name text box when I type in a new name, type *ESC*, and click to change the team name again', () => {
       // act
       clickToChangeTeamName('Team 1')
       cy.focused().type('{selectall}')
@@ -158,7 +158,7 @@ describe('Scoreboard app', () => {
       expectTeamNameTextBoxToHaveDefaultValue()
     })
 
-    it('should change the team name when I type a new name and then type *ENTER*', () => {
+    xit('should change the team name when I type a new name and then type *ENTER*', () => {
       // arrange
       clickToChangeTeamName('Team 1')
 
@@ -171,7 +171,7 @@ describe('Scoreboard app', () => {
       cy.contains(/^New name$/)
     })
 
-    it('should not allow XSS attacks in the name text box', () => {
+    xit('should not allow XSS attacks in the name text box', () => {
       // arrange
       const stub = cy.stub()
       cy.on('window:alert', stub)
@@ -195,12 +195,12 @@ describe('Scoreboard app', () => {
         cy.focused().type('{selectAll}{backspace}{enter}')
       })
 
-      it('should reset the name', () => {
+      xit('should reset the name', () => {
         // assert
         expectTeamNameToBeResetToDefault()
       })
 
-      it('should then re-set the name in the text box if you try to edit it again', () => {
+      xit('should then re-set the name in the text box if you try to edit it again', () => {
         // act
         clickToChangeTeamName('Team 1')
 
@@ -211,7 +211,7 @@ describe('Scoreboard app', () => {
   })
 
   describe('history', () => {
-    it('should display an "undo" button when I click add', () => {
+    xit('should display an "undo" button when I click add', () => {
       // act
       clickAddButton('Team 1')
 
@@ -219,7 +219,7 @@ describe('Scoreboard app', () => {
       cy.contains('button', 'Undo')
     })
 
-    it('should revert team 1 back to 0 if I press their add button 3 times, wait 3 seconds, and then press "undo"', () => {
+    xit('should revert team 1 back to 0 if I press their add button 3 times, wait 3 seconds, and then press "undo"', () => {
       // arrange
       cy.clock()
 
@@ -234,7 +234,7 @@ describe('Scoreboard app', () => {
       assertTeamAndScoreDisplayed('Team 1', 0)
     })
 
-    it('should revert team 1 back to 2 if I press their add button 2 times, wait 3 seconds, press their add button 2 more times, and then press "undo"', () => {
+    xit('should revert team 1 back to 2 if I press their add button 2 times, wait 3 seconds, press their add button 2 more times, and then press "undo"', () => {
       // arrange
       cy.clock()
 
@@ -251,7 +251,7 @@ describe('Scoreboard app', () => {
       assertTeamAndScoreDisplayed('Team 1', 2)
     })
 
-    it('should revert back two history items if I do 3 score changes, separated by 3 seconds each, and then press undo twice', () => {
+    xit('should revert back two history items if I do 3 score changes, separated by 3 seconds each, and then press undo twice', () => {
       // arrange
       cy.clock()
 
@@ -275,7 +275,7 @@ describe('Scoreboard app', () => {
       assertTeamAndScoreDisplayed('Team 1', 3)
     })
 
-    it('should revert my changes if I press the undo button immediately without waiting 3 seconds', () => {
+    xit('should revert my changes if I press the undo button immediately without waiting 3 seconds', () => {
       // act
       clickAddButton('Team 1')
       cy.contains('button', 'Undo').click()
@@ -284,7 +284,7 @@ describe('Scoreboard app', () => {
       assertTeamAndScoreDisplayed('Team 1', 0)
     })
 
-    it('should show a redo button when I press undo', () => {
+    xit('should show a redo button when I press undo', () => {
       // arrange
       clickAddButton('Team 1')
 
@@ -295,7 +295,7 @@ describe('Scoreboard app', () => {
       cy.contains('button', 'Redo')
     })
 
-    it('should disable the undo button if i have one history item and I click undo once', () => {
+    xit('should disable the undo button if i have one history item and I click undo once', () => {
       // arrange
       clickAddButton('Team 1')
 
@@ -306,7 +306,7 @@ describe('Scoreboard app', () => {
       cy.contains('button', 'Undo').should('be.disabled')
     })
 
-    it('should enable the undo button if I click add, click undo, and then click add again', () => {
+    xit('should enable the undo button if I click add, click undo, and then click add again', () => {
       // act
       clickAddButton('Team 1')
       cy.contains('button', 'Undo').click()
@@ -325,18 +325,18 @@ describe('Scoreboard app', () => {
         cy.contains('button', 'Undo').click()
       })
 
-      it('should allow me to undo a second time', () => {
+      xit('should allow me to undo a second time', () => {
         // assert
         assertTeamAndScoreDisplayed('Team 1', 0)
       })
   
-      it('should disable the undo button', () => {
+      xit('should disable the undo button', () => {
         // assert
         cy.contains('button', 'Undo').should('be.disabled')
       })
     })
 
-    it('should set the score to 1 if I click add, click undo, and click redo', () => {
+    xit('should set the score to 1 if I click add, click undo, and click redo', () => {
       // act
       clickAddButton('Team 1')
       cy.contains('button', 'Undo').click()
@@ -346,7 +346,7 @@ describe('Scoreboard app', () => {
       assertTeamAndScoreDisplayed('Team 1', 1)
     })
 
-    it('should disable the redo button click redo and get to the front of the history', () => {
+    xit('should disable the redo button click redo and get to the front of the history', () => {
       // act
       clickAddButton('Team 1')
       cy.contains('button', 'Undo').click()
@@ -356,7 +356,7 @@ describe('Scoreboard app', () => {
       cy.contains('button', 'Redo').should('be.disabled')
     })
 
-    it('should enable the undo button if I click add, click undo, and click redo', () => {
+    xit('should enable the undo button if I click add, click undo, and click redo', () => {
       // act
       clickAddButton('Team 1')
       cy.contains('button', 'Undo').click()
@@ -366,7 +366,7 @@ describe('Scoreboard app', () => {
       cy.contains('button', 'Undo').should('not.be.disabled')
     })
 
-    it('should enable the redo button if I click add, click undo, click redo, and click undo', () => {
+    xit('should enable the redo button if I click add, click undo, click redo, and click undo', () => {
       // act
       clickAddButton('Team 1')
       cy.contains('button', 'Undo').click()
@@ -377,7 +377,7 @@ describe('Scoreboard app', () => {
       cy.contains('button', 'Redo').should('not.be.disabled')
     })
 
-    it('should set the score to 2 if I click add twice, wait 3 seconds, click subtract, and click undo', () => {
+    xit('should set the score to 2 if I click add twice, wait 3 seconds, click subtract, and click undo', () => {
       // arrange
       cy.clock()
 
@@ -392,7 +392,7 @@ describe('Scoreboard app', () => {
       assertTeamAndScoreDisplayed('Team 1', 2)
     })
 
-    it('should set the score to 2-4 if I click add 2 times for team 1, click add 4 times for team 2, wait 3 seconds, click add 2 times for both teams, and click undo', () => {
+    xit('should set the score to 2-4 if I click add 2 times for team 1, click add 4 times for team 2, wait 3 seconds, click add 2 times for both teams, and click undo', () => {
       // arrange
       cy.clock()
 
@@ -415,7 +415,7 @@ describe('Scoreboard app', () => {
       assertTeamAndScoreDisplayed('Team 2', 4)
     })
 
-    it('should disable the redo button if I click add, click undo, and then click add', () => {
+    xit('should disable the redo button if I click add, click undo, and then click add', () => {
       // act
       clickAddButton('Team 1')
       cy.contains('button', 'Undo').click()
@@ -427,7 +427,7 @@ describe('Scoreboard app', () => {
   })
 
   describe('persistence', () => {
-    it('should keep my score if I click add for team 1 and then reload the page', () => {
+    xit('should keep my score if I click add for team 1 and then reload the page', () => {
       // act
       clickAddButton('Team 1')
       cy.reload()
@@ -436,7 +436,7 @@ describe('Scoreboard app', () => {
       assertTeamAndScoreDisplayed('Team 1', 1)
     })
 
-    it('should keep my history and display a score of 1 if I click add, wait 3 seconds, click add, reload the page, and click undo', () => {
+    xit('should keep my history and display a score of 1 if I click add, wait 3 seconds, click add, reload the page, and click undo', () => {
       // arrange
       cy.clock()
 
@@ -451,7 +451,7 @@ describe('Scoreboard app', () => {
       assertTeamAndScoreDisplayed('Team 1', 1)
     })
 
-    it('should keep my history and display a score of 1 if I click add, wait 3 seconds, and reload the page', () => {
+    xit('should keep my history and display a score of 1 if I click add, wait 3 seconds, and reload the page', () => {
       // arrange
       cy.clock()
 
@@ -464,7 +464,7 @@ describe('Scoreboard app', () => {
       assertTeamAndScoreDisplayed('Team 1', 1)
     })
 
-    it('should keep my team name when I reload the page', () => {
+    xit('should keep my team name when I reload the page', () => {
       // act
       clickToChangeTeamName('Team 1')
       cy.focused().type('{selectall}')
@@ -476,7 +476,7 @@ describe('Scoreboard app', () => {
       assertTeamAndScoreDisplayed('Moonshot', 0)
     })
 
-    it('should the score if I click "New game"', () => {
+    xit('should the score if I click "New game"', () => {
       // arrange
       cy.clock()
 
@@ -491,7 +491,7 @@ describe('Scoreboard app', () => {
     })
 
     const itShouldNotResetTheTeamNameWhenIClickNewGameFor = (team: string) => {
-      it(`should not reset the team name when I click "New game" for ${team}`, () => {
+      xit(`should not reset the team name when I click "New game" for ${team}`, () => {
         // act
         clickToChangeTeamName(team)
         cy.focused().type('{selectall}')
@@ -510,41 +510,41 @@ describe('Scoreboard app', () => {
   const getCardForTeam = (team: string) => cy.contains(team).closest('[role="listitem"]')
 
   describe('team styling', () => {
-    it('should display team 1 with a red background', () => {
+    xit('should display team 1 with a red background', () => {
       // assert
       getCardForTeam('Team 1').should('have.css', 'background-color').and('be.colored', '#bc2525')
     })
 
-    it('should display team 1 with white text', () => {
+    xit('should display team 1 with white text', () => {
       // assert
       getCardForTeam('Team 1').find('[role="heading"]').should('have.css', 'color').and('be.colored', '#fff')
       getCardForTeam('Team 1').find('footer button').should('have.css', 'color').and('be.colored', '#fff')
     })
 
-    it('should display team 2 with a blue background', () => {
+    xit('should display team 2 with a blue background', () => {
       getCardForTeam('Team 2').should('have.css', 'background-color').and('be.colored', '#2772db')
     })
 
-    it('should display team 2 with white text', () => {
+    xit('should display team 2 with white text', () => {
       // assert
       getCardForTeam('Team 2').find('[role="heading"]').should('have.css', 'color').and('be.colored', '#fff')
       getCardForTeam('Team 2').find('footer button').should('have.css', 'color').and('be.colored', '#fff')
     })
 
-    it('should display the score for team 1 as size 180px font', () => {
+    xit('should display the score for team 1 as size 180px font', () => {
       getCardForTeam('Team 1').find('[role="heading"][aria-level="2"]').should('have.css', 'font-size', '180px')
     })
 
-    it('should have a dark page background', () => {
+    xit('should have a dark page background', () => {
       cy.get('html').should('have.css', 'background-color', 'rgb(20, 22, 26)')
     })
 
-    it('should have the edit button for team 1 be danger-themed', () => {
+    xit('should have the edit button for team 1 be danger-themed', () => {
       getCardForTeam('Team 1').contains('button', 'Edit').should('have.css', 'color').and('be.colored', 'rgb(26, 0, 5)')
       getCardForTeam('Team 1').contains('button', 'Edit').should('have.css', 'background-color').and('be.colored', 'rgb(255, 102, 133)')
     })
 
-    it('should have the edit button for team 2 be info-themed', () => {
+    xit('should have the edit button for team 2 be info-themed', () => {
       getCardForTeam('Team 2').contains('button', 'Edit').should('have.css', 'color').and('be.colored', 'rgb(0, 36, 51)')
       getCardForTeam('Team 2').contains('button', 'Edit').should('have.css', 'background-color').and('be.colored', 'rgb(102, 209, 255)')
     })
